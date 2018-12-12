dfa44973fa5ac3a098ec976a546655d9509b6177
alvsgustavo <gustavo.daniel.alves@ccc.ufcg.edu.br>
2018-12-12 03:26:54 -0300

    funções adicionadas em usuário

--- src/main/java/br/com/edu/ufcg/cccfarma/api/controller/UsuariosController.java
+++ src/main/java/br/com/edu/ufcg/cccfarma/api/controller/UsuariosController.java
@@ -5,19 +5,40 @@ import java.util.List;
 import br.com.edu.ufcg.cccfarma.api.repository.UsuariosRepositorio;
 import br.com.edu.ufcg.cccfarma.api.model.Usuario;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/api")
 public class UsuariosController {
-	
+
 	@Autowired
 	private UsuariosRepositorio usuarios;
 
-	@GetMapping
-	public List<Usuario> listaUsuarios() {
-		return usuarios.findAll();
-	}
+	@PostMapping("/api/signup")
+    public ResponseEntity<?> cadastrar(@RequestBody Usuario novoUsuario) {
+	    usuarios.save(novoUsuario);
+	    return new ResponseEntity(novoUsuario, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/api/user/{cpf}")
+    public ResponseEntity<?> getUsuario(@PathVariable String cpf) {
+	    ResponseEntity response;
+	    Usuario usuario = usuarios.getOne(cpf);
+
+	    if (usuario == null) {
+	        response = new ResponseEntity(HttpStatus.NO_CONTENT);
+
+	    } else {
+	        response = new ResponseEntity(usuario, HttpStatus.OK);
+        }
+
+
+	    return response;
+    }
+
+
+
 }
--- src/main/java/br/com/edu/ufcg/cccfarma/api/model/Usuario.java
+++ src/main/java/br/com/edu/ufcg/cccfarma/api/model/Usuario.java
@@ -36,12 +36,68 @@ public class Usuario implements Serializable{
 	
 	@NotEmpty
 	private String endereco;
-	
-	@NotNull
-	private boolean admin;
 
-	@CreatedDate
-	@Temporal(TemporalType.TIMESTAMP)
-	private Date cadastro;
+    @NotNull
+    private boolean admin;
+
+    @CreatedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date cadastro;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public boolean isAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(boolean admin) {
+        this.admin = admin;
+    }
+
+    public Date getCadastro() {
+        return cadastro;
+    }
+
+    public void setCadastro(Date cadastro) {
+        this.cadastro = cadastro;
+    }
 
 }
